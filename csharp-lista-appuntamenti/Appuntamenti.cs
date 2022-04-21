e8da9699c001d1c82dc84786332e2799207c88ac
Alberto Rosa <alberto.r.93@hotmail.it>
2022-04-21 21:36:29 +0200

    isDataValida metod

--- csharp-lista-appuntamenti/Appuntamenti.cs
+++ csharp-lista-appuntamenti/Appuntamenti.cs
@@ -24,7 +24,7 @@ namespace lista_appuntamenti
         {
             try
             {
-                //metodo per vedere se la data è giusta (this.data);
+                isDataValida(this.data);
             }
             catch (Exception ex) //consigliata dal compiler et https://docs.microsoft.com/it-it/dotnet/csharp/language-reference/keywords/try-catch
             {
@@ -33,9 +33,28 @@ namespace lista_appuntamenti
 
             this.nome = nome;
             this.luogo = luogo;
-            this.data = data;
+           // this.data = data; inutile quando avrò il metodo nel try
 
         }
+
+
+        //metodo interno per controllare la validità della data
+
+        internal void isDataValida(DateTime data)
+        {
+            if (data == null) //why "a value of datatime is never equal to null"?
+            {
+                throw new ArgumentNullException("data");
+            } else if (data < DateTime.Now)
+            {
+                throw new ArgumentOutOfRangeException("Data antecedente a quella odierna");
+            } else
+            {
+                this.data = data;
+            }
+        }
+
+
 
         //metodo per cambiare Date "dove riceverà un nuovo oggetto di tipo DateTime" + exception se antecedente
 
