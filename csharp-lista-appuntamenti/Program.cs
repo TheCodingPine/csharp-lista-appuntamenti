81e9d05d4be8e1a5b07a1210efc7dfa576a205fb
Alberto Rosa <alberto.r.93@hotmail.it>
2022-04-21 20:16:53 +0200

    Add project files.

---
+++ csharp-lista-appuntamenti/Program.cs
@@ -0,0 +1,11 @@
+﻿
+/*In questo esercizio vogliamo creare una sorta di nostra agenda personale dove vogliamo salvare i nostri futuri appuntamenti. Per tanto il programma si dovrà occupare
+ * di salvare le informazioni in una lista di appuntamenti.
+ * 
+
+
+A questo punto nel programma principale Program.cs creiamo una Lista di Appuntamenti vuota e chiediamo all’utente quanti appuntamenti vuole inserire.
+
+Per ogni appuntamento chiediamo le informazioni necessarie e inizializziamo un oggetto Appuntamento, stando attenti a gestire eventuali eccezioni,
+ossia se uno sbaglia quell'appuntamento deve essere ri-richiesto. Inseriamo l’oggetto appuntamento correttamente inizializzato nella lista.
+Alla fine stampate in maniera user-friendly (usando possibilmente l'override del metodo ToString(); della classe Object) la lista dei vostri appuntamenti futuri.*/
