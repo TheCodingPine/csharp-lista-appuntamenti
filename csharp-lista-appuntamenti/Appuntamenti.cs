276c7e27f5da67e08c279ee0c6890cd3183564b0
Alberto Rosa <alberto.r.93@hotmail.it>
2022-04-21 20:20:55 +0200

    setup

--- csharp-lista-appuntamenti/Appuntamenti.cs
+++ csharp-lista-appuntamenti/Appuntamenti.cs
@@ -7,21 +7,30 @@ using System.Threading.Tasks;
 /*A tale scopo dovremmo creare una classe Appuntamento che si occupa di salvare e gestire (costruttore e metodi opportuni) eventuali appuntamenti.
 La classe Appuntamento avrà:
 
-> un attributo data e ora (DateTime)
-> un attributo nome
-> un attributo localitaAppuntamento
-> un costruttore che inizializza tutti i campi di Appuntamento, ma che controlli che un Appuntamento non venga aggiunto nel passato, cioè prima di oggi.
-Nel caso lancia un’eccezione.
-
->un metodo per cambiare la Data dell’appuntamento dove riceverà un nuovo oggetto di tipo DateTime. Il metodo dovrà occuparsi di lanciare opportune eccezioni
-nel caso in cui la data sia antecedente ad oggi.
 
 >Tutti gli eventuali altri Getters e Setters*/
 
-namespace csharp_lista_appuntamenti
+namespace lista_appuntamenti
 {
     internal class Appuntamenti
     {
+        private string nome;
+        private string localitaAppuntamento;
+        public DateTime;
+
+        //costruttore che inizializza i campi e controlli che la data non sia antecedente + exception
+
+        //metodo per cambiare Date "dove riceverà un nuovo oggetto di tipo DateTime" + exception se amtecedente
+
+
+
+
+
+
+
+
+
+
 
     }
 }
